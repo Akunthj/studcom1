dc585e652dcdc5f1dc6f892260618fe530e6b2b8
shashwatmishra0104-hub <shashwatmishra0104@gmail.com>
2026-02-06 16:56:16 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
+import { SubjectProvider } from '@/contexts/SubjectContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SubjectProvider>
+      <App />
+    </SubjectProvider>
   </StrictMode>
 );
